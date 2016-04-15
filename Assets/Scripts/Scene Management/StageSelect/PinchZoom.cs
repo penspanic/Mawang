fac5290fd9a7bb47a2f8c716a5ae95fd8d5c01b2
Geun Hee Park <penspanic@gmail.com>
2016-04-15 18:22:26 +0900

    Stage Select pinch zoom, drag

---
+++ Assets/Scripts/Scene Management/StageSelect/PinchZoom.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PinchZoom : MonoBehaviour
+{
+    public float perspectiveZoomSpeed;     // The rate of change of the field of view in perspective mode.
+    public float orthoZoomSpeed;           // The rate of change of the orthographic size in orthographic mode.
+
+
+    void Update()
+    {
+        // If there are two touches on the device...
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // If the camera is orthographic...
+            if (Camera.main.orthographic)
+            {
+                // ... change the orthographic size based on the change in distance between the touches.
+                Camera.main.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+                // Make sure the orthographic size never drops below zero.
+                Camera.main.orthographicSize = Mathf.Max(Camera.main.orthographicSize, 3.6f);
+
+                Camera.main.orthographicSize = Mathf.Min(Camera.main.orthographicSize, 7.2f);
+            }
+            else
+            {
+                // Otherwise change the field of view based on the change in distance between the touches.
+                Camera.main.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+                // Clamp the field of view to make sure it's between 0 and 180.
+                Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 0.1f, 179.9f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Scene Management/StageSelect/StageSelect_MapScroll.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageSelect_MapScroll : MonoBehaviour
+{
+    bool prevMouseDown = false;
+
+    Vector2 prevTouchPos = Vector2.zero;
+    Vector2 currTouchPos = Vector2.zero;
+    Vector2 deltaPos = Vector2.zero;
+
+    void Update()
+    {
+        if (Input.touchCount == 2)
+            return;
+        currTouchPos = Input.mousePosition;
+
+        deltaPos = prevTouchPos - currTouchPos;
+        prevTouchPos = currTouchPos;
+
+        if (Input.GetMouseButton(0) && prevMouseDown)
+        {
+            float moveValue = deltaPos.x * Time.deltaTime;
+
+            Vector3 newPos = transform.position;
+            newPos.x = Mathf.Clamp(newPos.x + moveValue, 0, GetMaxPosX());
+            transform.position = newPos;
+
+            prevMouseDown = true;
+        }
+        if (Input.GetMouseButton(0))
+            prevMouseDown = true;
+        else
+            prevMouseDown = false;
+    }
+
+    float GetMaxPosX()
+    {
+        return 12.8f;
+    }
+
+    float GetMaxPosY()
+    {
+        return 7.2f;
+    }
+}
